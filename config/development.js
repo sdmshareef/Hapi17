36f6b5f0cbac5b7a8c0782d70f3acf857aa81fa4
sdmshareef <sdmshreef@gmail.com>
2018-02-21 16:46:29 +0530

    Hapi 17 boilerplate

---
+++ config/development.js
@@ -0,0 +1,15 @@
+module.exports = {
+  server: {
+    port: 4000,
+  },
+  jwt: {
+    secret: 'SUPER-SECRET',
+  },
+  database: {
+    connection: {
+      database: 'kshetr_tracker',
+      user: 'root',
+      password: '',
+    },
+  },
+};
